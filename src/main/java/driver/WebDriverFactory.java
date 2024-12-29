c31fd3a52f98f342b5b74364dad158b622ea84f7
KURU <svrtik_nia@mail.ru>
2024-12-29 13:45:01 +0500

    Домашнее задание ВебДрайвер Фабрика

---
+++ src/main/java/driver/WebDriverFactory.java
@@ -0,0 +1,39 @@
+package driver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class WebDriverFactory {
+
+    public WebDriver createDriver(Browsers browser, String... options) {
+
+        switch (browser) {
+            case CHROME -> {
+                ChromeOptions option = new ChromeOptions();
+                option.addArguments(options);
+                WebDriverManager.chromedriver().setup();
+                return new ChromeDriver(option);
+            }
+            case FOX -> {
+                FirefoxOptions option = new FirefoxOptions();
+                option.addArguments(options);
+                WebDriverManager.firefoxdriver().setup();
+                return new FirefoxDriver(option);
+            }
+            case EDGE -> {
+                EdgeOptions option = new EdgeOptions();
+                option.addArguments(options);
+                WebDriverManager.edgedriver().setup();
+                return new EdgeDriver(option);
+            }
+        }
+        return null;
+    }
+
+}
--- src/test/java/autotest/RegisterFormTest.java
+++ src/test/java/autotest/RegisterFormTest.java
@@ -1,7 +1,7 @@
 package autotest;
 
 import driver.Browsers;
-import driver.GetDriver;
+import driver.WebDriverFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterEach;
@@ -9,10 +9,10 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+import pages.RegistrationPage;
 
 import java.awt.*;
 import java.util.Objects;
@@ -22,25 +22,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RegisterFormTest {
 
     private WebDriver driver;
+    RegistrationPage registrationPage;
     private static String name;
     private static String password;
     private static String mail;
     private static String birthday;
     private static String birthdayAssert;
-    private static final Browsers BROWSER = Browsers.FOX;
-    private static String url;
+    private static final Browsers BROWSER =
+            Browsers.valueOf(System.getProperty("browser", Browsers.FOX.name()).toUpperCase());
 
     private final Logger logger = LogManager.getLogger(RegisterFormTest.class);
 
     @BeforeAll
     static void beforeAll() {
-        url = System.getProperty("base.url");
         name = System.getProperty("login");
         password = System.getProperty("password");
         mail = System.getProperty("mail");
         birthday = System.getProperty("birthday");
         birthdayAssert = System.getProperty("birthdayAssert");
-        Objects.requireNonNull(url);
         Objects.requireNonNull(name);
         Objects.requireNonNull(password);
         Objects.requireNonNull(mail);
@@ -50,7 +49,8 @@ public class RegisterFormTest {
 
     @BeforeEach
     void setUp() {
-        this.driver = new GetDriver().getDriver(BROWSER);
+        this.driver = new WebDriverFactory().createDriver(BROWSER);
+        this.registrationPage = new RegistrationPage(driver);
     }
 
     @ParameterizedTest
@@ -62,24 +62,18 @@ public class RegisterFormTest {
                     4,native,Носитель языка
                     """)
     public void registrationForm(int elementIndex, String elementValue, String elementName) throws AWTException {
-        driver.get(url);
+        registrationPage.open();
 
-        WebElement userName = driver.findElement(By.id("username"));
-        userName.sendKeys(name);
+        WebElement userName = registrationPage.sendUserName(name);
         assertEquals(name, userName.getDomProperty("value"));
         logger.info("Введеное имя соответствует содержимому поля.");
 
-        WebElement userMail = driver.findElement(By.id("email"));
-        userMail.sendKeys(mail);
+        WebElement userMail = registrationPage.sendUserMail(mail);
         assertEquals(mail, userMail.getDomProperty("value"));
         logger.info("Введеный email соответствует содержимому поля.");
 
-        WebElement userPassword = driver.findElement(By.id("password"));
-        userPassword.sendKeys(password);
-
-        WebElement passwordConf = driver.findElement(By.id("confirm_password"));
-        passwordConf.sendKeys(password);
-
+        WebElement userPassword = registrationPage.sendUserPassword(password);
+        WebElement passwordConf = registrationPage.sendPasswordConf(password);
         assertEquals(password, userPassword.getDomProperty("value"));
         logger.info("Введеный пароль соответствует эталону.");
         assertEquals(
@@ -88,8 +82,7 @@ public class RegisterFormTest {
         );
         logger.info("Содержимое поля 'Пароль' и 'Подтвердите пароль' соответствуют друг другу.");
 
-        WebElement berthDay = driver.findElement(By.id("birthdate"));
-        berthDay.click();
+        WebElement berthDay = registrationPage.clickBerthDay();
         Robot r = new Robot();
         r.setAutoDelay(100);
         for (char c : birthday.toCharArray()) {
@@ -98,22 +91,19 @@ public class RegisterFormTest {
         assertEquals(birthdayAssert, berthDay.getDomProperty("value"));
         logger.info("Введеная дата рождения соответствует эталону: " + birthdayAssert);
 
-        Select langLevel = new Select(driver.findElement(By.id("language_level")));
-        langLevel.selectByIndex(elementIndex);
+        Select langLevel = registrationPage.selectLangLevel(elementIndex);
         WebElement element = langLevel.getFirstSelectedOption();
         assertEquals(elementValue, element.getDomProperty("value"));
         assertEquals(elementName, element.getText());
         logger.info("Выбранный уровень языка соответствует эталону: " + elementName);
 
-        WebElement registrationBtn = driver.findElement(By.cssSelector("input[type=submit]"));
-        registrationBtn.click();
-
+        registrationPage.clickRegistrationBtn();
         assertEquals(
                 ("Имя пользователя: %s\n" +
                  "Электронная почта: %s\n" +
                  "Дата рождения: %s\n" +
                  "Уровень языка: %s").formatted(name, mail, birthdayAssert, elementValue),
-                driver.findElement(By.cssSelector("div#output")).getText()
+                registrationPage.textOutputText()
         );
         logger.info("Форма соответствует выведенному тексту.");
     }
